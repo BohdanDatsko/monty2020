318175474f0c97f8336760b23e2287b08191f337
Bohdan Datsko <aborsen1072@gmail.com>
2019-09-22 12:22:52 +0300

    Fix DEBUG

--- monty2019/settings.py
+++ monty2019/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # DEBUG = False
-# DEBUG = config("DEBUG", default=False, cast=bool)
-DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
+DEBUG = config("DEBUG", default=False, cast=bool)
+# DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
 
 ALLOWED_HOSTS = ["monty2019.herokuapp.com"]
 
