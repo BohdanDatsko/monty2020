e76348262163a5ee7e2b120e7c679f0898e3d254
BilyiVoron <aborsen1072@gmail.com>
2019-10-21 21:01:14 +0300

    d60 v83

--- monty2019/settings.py
+++ monty2019/settings.py
@@ -308,28 +308,28 @@ CORS_ALLOW_CREDENTIALS = True
 # ------------------------------------------------------------------------------
 django_heroku.settings(locals())
 
-# # Create Django-Logs
-# # ------------------------------------------------------------------------------
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "formatters": {
-#         "verbose": {
-#             "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-#             "datefmt": "%d/%b/%Y %H:%M:%S",
-#         },
-#         "simple": {"format": "%(levelname)s %(message)s"},
-#     },
-#     "handlers": {
-#         "file": {
-#             "level": "DEBUG",
-#             "class": "logging.FileHandler",
-#             "filename": "monty2019.log",
-#             "formatter": "verbose",
-#         }
-#     },
-#     "loggers": {
-#         "django": {"handlers": ["file"], "propagate": True, "level": "DEBUG"},
-#         "monty": {"handlers": ["file"], "level": "DEBUG"},
-#     },
-# }
+# Create Django-Logs
+# ------------------------------------------------------------------------------
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "monty2019.log",
+            "formatter": "verbose",
+        }
+    },
+    "loggers": {
+        "django": {"handlers": ["file"], "propagate": True, "level": "DEBUG"},
+        "monty": {"handlers": ["file"], "level": "DEBUG"},
+    },
+}
